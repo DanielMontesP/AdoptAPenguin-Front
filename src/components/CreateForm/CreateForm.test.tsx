d51456e124abdb9ce29c810cfe323e7dcaa253ab
Daniel Montes <daniel.montes@gmail.com>
2022-09-05 05:45:33 +0200

    Fix sonarcloud smells

--- src/components/CreateForm/CreateForm.test.tsx
+++ src/components/CreateForm/CreateForm.test.tsx
@@ -3,6 +3,7 @@ import userEvent from "@testing-library/user-event";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import store from "../../app/redux/store/store";
+import { deletePenguinThunk } from "../../app/redux/thunks/penguinThunk/penguinThunk";
 import { mockPenguin, mockPenguins } from "../../mocks/penguins";
 import CreateForm from "./CreateForm";
 
@@ -48,6 +49,41 @@ describe("Given a CreateForm component", () => {
       handlImg(mockEvent);
 
       expect(handlImg).toHaveBeenCalled();
+    });
+  });
+  describe("When processCreate is called", () => {
+    test("Then the two inputs should be empty", () => {
+      const button = "bt-save";
+      const imageLabel = "image-input";
+      const handleSubmit = jest.fn();
+      const processCreate = jest.fn();
+      const handleImg = jest.fn();
+
+      const inputText = "user1";
+      render(
+        <Provider store={store}>
+          <BrowserRouter>
+            <CreateForm penguin={mockPenguin} />
+          </BrowserRouter>
+        </Provider>
+      );
+      const label = screen.getByPlaceholderText(button);
+      const image = screen.getByPlaceholderText(imageLabel);
+      document.location.href = jest.fn().mockReturnValue("create").toString();
+      userEvent.type(image, inputText);
+      userEvent.click(label);
+
+      expect(label).toBeInTheDocument();
+      const dispatch = jest.fn();
+      const mockEvent = { event: { target: { files: [mockPenguins] } } };
+      handleSubmit(mockEvent);
+      processCreate("New");
+      handleImg();
+
+      dispatch(deletePenguinThunk);
+      expect(handleSubmit).toHaveBeenCalled();
+      expect(processCreate).toHaveBeenCalled();
+      expect(dispatch).toHaveBeenCalled();
     });
   });
 });
