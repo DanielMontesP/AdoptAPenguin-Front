e2e0fbdcdb4d31423fc45def45588606c271b953
Daniel Montes <daniel.montes@gmail.com>
2022-09-13 01:25:45 +0200

    Fix sonarcloud smells

--- src/components/Penguins/Penguins.tsx
+++ src/components/Penguins/Penguins.tsx
@@ -1,9 +1,7 @@
 import Penguin from "../Penguin/Penguin";
 import { finishedLoadingActionCreator } from "../../app/redux/features/uiSlice/uiSlice";
-
 import { IPenguin } from "../../app/redux/types/penguin/penguinInterfaces";
 import PagesStyles from "../../Styles/PagesStyles";
-// import { useAppSelector } from "../../app/redux/hooks/hooks";
 
 const loadedState = finishedLoadingActionCreator();
 const hidderDelete = loadedState ? "" : " display-none";
@@ -13,11 +11,6 @@ interface Props {
 }
 
 const Penguins = ({ allPenguins }: Props): JSX.Element => {
-  // const { modalType } = useAppSelector((state) => state.ui);
-
-  // const classSearchTitle =
-  //   modalType === "FFeature" ? "search-title" : "display-none";
-
   return (
     <PagesStyles
       className={`penguins-container${hidderDelete}`}
