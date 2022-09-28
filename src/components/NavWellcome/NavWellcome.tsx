52840ec87902a67a4e4a1a1ca33ac249a83cc30e
Daniel Montes <daniel.montes@gmail.com>
2022-09-28 19:16:33 +0200

    Fix style menu logout hidder tests p4

--- src/components/NavWellcome/NavWellcome.tsx
+++ src/components/NavWellcome/NavWellcome.tsx
@@ -4,7 +4,9 @@ const NavWellcome = (): JSX.Element => {
   return (
     <div className={`header-wellcome`}>
       <div className="header-title-container">
-        <h1>AdoptApenguin.com</h1>
+        <h1 className={`header-desktop-title1`}>Responsive site</h1>
+        <h1 className={`header-desktop-title3`}>AdoptApenguin.com</h1>
+        <h1 className={`header-desktop-title2`}>Amazing features...</h1>
       </div>
     </div>
   );
